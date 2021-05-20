ede4694468f3e7b093a4bedd4fe41aae027cb672
JungBok Cho <chojb2015@gmail.com>
2021-05-19 22:56:51 -0700

    Add showRecipe in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { SearchByIngredientsComponent } from './search-by-ingredients/search-by-ingredients.component';
 import { SearchByCuisineComponent } from './search-by-cuisine/search-by-cuisine.component';
+import { ShowRecipeComponent } from './show-recipe/show-recipe.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'byIngredient', component: SearchByIngredientsComponent },
-  { path: 'byCuisine', component: SearchByCuisineComponent }
+  { path: 'byCuisine', component: SearchByCuisineComponent },
+  { path: 'showRecipe', component: ShowRecipeComponent }
 ];
 
 @NgModule({
