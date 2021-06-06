53a7cb276a35c32d3ecdb1408402a8fb42d14e52
JungBok Cho <chojb2015@gmail.com>
2021-06-06 00:54:22 -0700

    Update app.component for SSO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserApiService } from './user-api.service';
 
 @Component({
   selector: 'app-root',
@@ -9,65 +10,83 @@ import { Router } from '@angular/router';
 export class AppComponent  {
  
   title = 'FridgeBuddy';
-  DisplayLogIn:boolean = true;
-  DisplayLogout:boolean = true;
-  currentUser:string = "Login / Sign Up";
+  isLoggedIn:boolean = false; 
+  currentUser:string;
 
-  constructor(public router : Router) {
-     this.DisplayLogIn = false;
-     this.DisplayLogout = false;
+  constructor(public router : Router, private auth: UserApiService) {
+    console.log("app.component.ts: " + this.isLoggedIn); 
+    this.auth.getStatus().subscribe(res => {
+      this.isLoggedIn = (/true/i).test(res);
+      console.log("Inside here " + this.isLoggedIn);
+      if(this.isLoggedIn){
+        this.auth.getUserProfile().subscribe(data => {
+          this.currentUser = JSON.stringify(data).replace(/"/g,"");
+          this.currentUser = 'Hi, '+ this.currentUser;
+        })
+      } else {
+        this.currentUser = "Login";
+        this.isLoggedIn = false;
+      }
+    })
   }
 
-  GetuserNameVal(name:string) {
-    if(name) {
-      this.currentUser = "Hi "+ name + "!";
-    }
-    this.DisplayLogIn = false;
-  }
+  // logoutUser(status: string) {
+  //   this.isLoggedIn = (/true/i).test(status);
+  // }
 
-  logMeOut(user:string){
-    this.currentUser = user;
-    if(this.currentUser != "Login / Sign Up")
-    {
-      this.DisplayLogout = true;
-    }
-    else{
-      this.DisplayLogout = false;
-      this.router.navigate(['/home']);
-    }
-  }
+  // GetuserNameVal(name: string) {
+  //   this.currentUser = "Hi "+ name + "!";
+  // }
+
+  // GetuserNameVal(name:string) {
+  //   if(name) {
+  //     this.currentUser = "Hi "+ name + "!";
+  //   }
+  //   this.DisplayLogIn = false;
+  // }
+
+  // logMeOut(user:string){
+  //   this.currentUser = user;
+  //   if(this.currentUser != "Login / Sign Up")
+  //   {
+  //     this.DisplayLogout = true;
+  //   }
+  //   else{
+  //     this.DisplayLogout = false;
+  //     this.router.navigate(['/home']);
+  //   }
+  // }
 
   ChangeLoginStats() {
     if(this.currentUser != "Login / Sign Up") {
-      this.DisplayLogIn = false;
-      this.DisplayLogout = true;
+      // name:string = 
       this.router.navigate(['/account']);
     } else {
-       this.DisplayLogIn = true;
-       this.DisplayLogout = false;
+       this.currentUser = "Login / Sign Up";
+       this.router.navigate(['/login']);
     }
   }
 
-  ChangeCusinStats() {
-    if(this.currentUser == "Login / Sign Up") {
-      this.DisplayLogIn = false;
-    } 
-    this.DisplayLogout = false;
-  }
+  // ChangeCusinStats() {
+  //   if(this.currentUser == "Login / Sign Up") {
+  //     // this.DisplayLogIn = false;
+  //   } 
+  //   // this.DisplayLogout = false;
+  // }
   
-  ChangeIngeredientStats() {
-    if(this.currentUser == "Login / Sign Up") {
-      this.DisplayLogIn = false;
-    } 
-    this.DisplayLogout = false;
-  }
+  // ChangeIngeredientStats() {
+  //   if(this.currentUser == "Login / Sign Up") {
+  //     // this.DisplayLogIn = false;
+  //   } 
+  //   // this.DisplayLogout = false;
+  // }
 
-  ChangeHomePageStats() {
-    if(this.currentUser == "Login / Sign Up") {
-      this.DisplayLogIn = false;
-    }
-    this.DisplayLogout = false;
+  // ChangeHomePageStats() {
+  //   if(this.currentUser == "Login / Sign Up") {
+  //     // this.DisplayLogIn = false;
+  //   }
+  //   // this.DisplayLogout = false;
 
-  }
+  // }
  
 }
