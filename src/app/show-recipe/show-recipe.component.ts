ca0d2c99202e38dbe13662d973e4b3e4461186d2
Ruifeng Wang <rwang@seattleu.edu>
2021-06-06 19:13:31 -0700

    Update show recipe page to post reviews

--- src/app/show-recipe/show-recipe.component.ts
+++ src/app/show-recipe/show-recipe.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {RecipeApiService} from '../recipe-api-service'
 import { ActivatedRoute } from '@angular/router';
+import { ReviewApiService } from '../review-api.service';
+import { Router } from '@angular/router';
+import { UUID } from 'angular2-uuid';
 
 @Component({
   selector: 'app-show-recipe',
@@ -9,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ShowRecipeComponent implements OnInit {
   public recipeApiService: RecipeApiService;
+  public reviewApiService: ReviewApiService;
   public recipe: any;
   public reviews: any;
 
@@ -17,8 +21,9 @@ export class ShowRecipeComponent implements OnInit {
 
   starRating = 0;
 
-  constructor(service: RecipeApiService, private route: ActivatedRoute) { 
+  constructor(service: RecipeApiService, private route: ActivatedRoute, reviewService: ReviewApiService, public router : Router) { 
     this.recipeApiService = service;
+    this.reviewApiService = reviewService;
 
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id'];
@@ -36,6 +41,22 @@ export class ShowRecipeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  sendReview(userComment:string, starRating:any) {
+    const body = {
+      "reviewId": UUID.UUID(),
+      "comment": userComment,
+      "writer": "abc",
+      "date": new Date().toLocaleDateString(),
+      "rate": starRating
+    }
+    this.reviewApiService.postReview(body, this.id).subscribe(async res =>
+    {
+      console.log('Successfully posted');
+      await this.router.navigate(['/showRecipe/' + this.id]);
+      location.reload();
+    });
   }
 
 }
